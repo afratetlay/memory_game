7b85a7a859aa3b8998a3b9466deef29a14a867dc
afratetlay <afratetlay@gmail.com>
2020-09-09 09:26:36 +0000

    i have found a bug in my data.js, i have fixed it

--- data.js
+++ data.js
@@ -1,77 +1,83 @@
 const cardData = [
-  {
-    name: "Bear",
-    image: "bear_mask.png",
-    id: "bear"
-  },
-  {
-    name: "Bee",
-    image: "bee_mask.png",
-    id: "bee"
-  },
-  {
-    name: "Mask",
-    image: "boy_with_mask.png",
-    id: "mask"
-  },
-  {
-    name: "Crown",
-    image: "crown_hair.png",
-    id: "crown"
-  },
-  {
-    name: "Girl",
-    image: "girl_with_mask.png",
-    id: "girl"
-  },
-  {
-    name: "Red",
-    image: "mask_red.png",
-    id: "red"
-  },
-  {
-    name: "Mohawk",
-    image: "mohawk_mask.png",
-    id: "mohawk"
-  },
-  {
-    name: "Yellow",
-    image: "yellow_mask.png",
-    id: "yellow"
-  },
-  {
-    name: "Blue",
-    image: "blue_mask.png",
-    id: "blue"
-  },
-  {
-    name: "Captain",
-    image: "captain_mask.png",
-    id: "captain"
-  },
-  {
-    name: "Red",
-    image: "red_mask.png",
-    id: "red"
-  },
-  {
-    name: "Green",
-    image: "green_mask.png",
-    id: "green"
-  }
+ {
+   name: "BearMask",
+   image: "bear_mask.png",
+   id: "bearmask"
+ },
+ {
+   name: "BeeMask",
+   image: "bee_mask.png",
+   id: "beemask"
+ },
+ {
+   name: "BlueMask",
+   image: "blue_mask.png",
+   id: "bluemask"
+ },
+ {
+   name: "BoyMask",
+   image: "boy_mask.png",
+   id: "boymask"
+ },
+ {
+   name: "CaptainMask",
+   image: "captain_mask.png",
+   id: "captainmask"
+ },
+ {
+   name: "CrownHair",
+   image: "crown_hair.png",
+   id: "crownhair"
+ },
+ {
+   name: "GirlMask",
+   image: "girl_mask.png",
+   id: "girlmask"
+ },
+ {
+   name: "GreenMask",
+   image: "green_mask.png",
+   id: "greenmask"
+ },
+ {
+   name: "IronMan",
+   image: "iron_man.png",
+   id: "ironman"
+ },
+ {
+   name: "SpiderMan",
+   image: "spider_man.png",
+   id: "spiderman"
+ },
+ {
+   name: "MohawkMask",
+   image: "mohawk_mask.png",
+   id: "mohawkmask"
+ },
+ {
+   name: "EyeMask",
+   image: "eye_mask.png",
+   id: "eyemask"
+ }
 ];
 
 const gameLevels = {
-  easy: {
-    class: "easy",
-    pairs: 6
-  },
-  medium: {
-    class: "medium",
-    pairs: 8
-  },
-  hard: {
-    class: "hard",
-    pairs: 12
-  }
+ easy: {
+   class: "easy",
+   pairs: 6,
+   twoStar: 6,
+   oneStar: 10
+ },
+ medium: {
+   class: "medium",
+   pairs: 8,
+   twoStar: 10,
+   oneStar: 14
+ },
+ hard: {
+   class: "hard",
+   pairs: 12,
+   twoStar: 16,
+   oneStar: 24
+ }
 };
